6c79fb4fe51dc37419e48fb00dd9dbefc6b2f9b7
niczgalvao <nicolas.galvao.oliveira@escola.pr.gov.br>
2025-11-10 09:31:36 -0300

    Add files via upload

---
+++ melhor do mundo (2)/melhor do mundo/melhor do mundo/mapdata.js
@@ -0,0 +1,402 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    BRAC: {
+      name: "Acre",
+      color: "#47ef10",
+      hover_color: "#1c5709",
+      description: "Estado da região Norte"
+    },
+    BRAL: {
+      name: "Alagoas",
+      color: "#1ce9d4",
+      hover_color: "#0b8990",
+      description: "Estado da região Nordeste"
+    },
+    BRAM: {
+      name: "Amazonas",
+      color: "#47ef10",
+      hover_color: "#1c5709",
+      description: "Estado da região Norte"
+    },
+    BRAP: {
+      name: "Amapá",
+      color: "#47ef10",
+      hover_color: "#1c5709",
+      description: "Estado da região Norte"
+    },
+    BRBA: {
+      name: "Bahia",
+      color: "#1ce9d4",
+      hover_color: "#0b8990",
+      description: "Estado da região Nordeste"
+    },
+    BRCE: {
+      name: "Ceará",
+      color: "#1ce9d4",
+      hover_color: "#0b8990",
+      description: "Estado da região Nordeste"
+    },
+    BRDF: {
+      name: "Distrito Federal",
+      color: "#e9ed14",
+      hover_color: "#788809",
+      description: "Estado da região Centro-Oeste"
+    },
+    BRES: {
+      name: "Espírito Santo",
+      color: "#eda714",
+      hover_color: "#b58106",
+      description: "Estado da região Sudeste"
+    },
+    BRGO: {
+      name: "Goiás",
+      color: "#e9ed14",
+      hover_color: "#788809",
+      description: "Estado da região Centro-Oeste"
+    },
+    BRMA: {
+      name: "Maranhão",
+      color: "#1ce9d4",
+      hover_color: "#0b8990",
+      description: "Estado da região Nordeste"
+    },
+    BRMG: {
+      name: "Minas Gerais",
+      color: "#eda714",
+      hover_color: "#b58106",
+      description: "Estado da região Sudeste"
+    },
+    BRMS: {
+      name: "Mato Grosso do Sul",
+      color: "#e9ed14",
+      hover_color: "#788809",
+      description: "Estado da região Centro-Oeste"
+    },
+    BRMT: {
+      name: "Mato Grosso",
+      color: "#e9ed14",
+      hover_color: "#788809",
+      description: "Estado da região Centro-Oeste"
+    },
+    BRPA: {
+      name: "Pará",
+      color: "#47ef10",
+      hover_color: "#1c5709",
+      description: "Estado da região Norte"
+    },
+    BRPB: {
+      name: "Paraíba",
+      color: "#1ce9d4",
+      hover_color: "#0b8990",
+      description: "Estado da região Nordeste"
+    },
+    BRPE: {
+      name: "Pernambuco",
+      color: "#1ce9d4",
+      hover_color: "#0b8990",
+      description: "Estado da região Nordeste"
+    },
+    BRPI: {
+      name: "Piauí",
+      color: "#1ce9d4",
+      hover_color: "#0b8990",
+      description: "Estado da região Sudeste"
+    },
+    BRPR: {
+      name: "Paraná",
+      color: "#ed1414",
+      hover_color: "#8c1313",
+      description: "Estado da região Sul"
+    },
+    BRRJ: {
+      name: "Rio de Janeiro",
+      color: "#eda714",
+      hover_color: "#b58106",
+      description: "Estado da região Sudeste"
+    },
+    BRRN: {
+      name: "Rio Grande do Norte",
+      color: "#1ce9d4",
+      hover_color: "#0b8990",
+      description: "Estado da região Nordeste"
+    },
+    BRRO: {
+      name: "Rondônia",
+      color: "#47ef10",
+      hover_color: "#1c5709",
+      description: "Estado da região Norte"
+    },
+    BRRR: {
+      name: "Roraima",
+      color: "#47ef10",
+      hover_color: "#1c5709",
+      description: "Estado da região Norte"
+    },
+    BRRS: {
+      name: "Rio Grande do Sul",
+      color: "#ed1414",
+      hover_color: "#8c1313",
+      description: "Estado da região Sul"
+    },
+    BRSC: {
+      name: "Santa Catarina",
+      color: "#ed1414",
+      hover_color: "#8c1313",
+      description: "Estado da região Sul"
+    },
+    BRSE: {
+      name: "Sergipe",
+      color: "#1ce9d4",
+      hover_color: "#0b8990",
+      description: "Estado da região Nordeste"
+    },
+    BRSP: {
+      name: "São Paulo",
+      color: "#eda714",
+      hover_color: "#b58106",
+      description: "Estado da região Sudeste"
+    },
+    BRTO: {
+      name: "Tocantins",
+      color: "#47ef10",
+      hover_color: "#1c5709",
+      description: "Estado da região Norte"
+    }
+  },
+  locations: {
+    "0": {
+      name: "Centro Oeste",
+      lat: "-15.783333",
+      lng: "-52.916667",
+      color: "#000000",
+      size: "30",
+      type: "circle",
+      url: "https://youtu.be/agH2bBnNUCs?si=z-fwcUbCYCi7E7SE",
+      description: "Desigualdade ligada à concentração de terras (latifúndios) e ao modelo do agronegócio."
+    },
+    "1": {
+      name: "Sul",
+      lat: "-27.0000",
+      lng: "-51.0000",
+      color: "#000000",
+      size: "30",
+      type: "circle",
+      description: "Embora tenha os melhores indicadores médios, apresenta desigualdades dentro da própria região, entre áreas urbanas desenvolvidas e zonas rurais mais pobres."
+    },
+    "2": {
+      name: "Nordeste",
+      lng: "-40.000",
+      lat: "-8.000",
+      color: "#000000",
+      size: "30",
+      type: "circle",
+      description: "Marcado pela pobreza estrutural e desigualdade de renda histórica, com alta dependência de programas sociais."
+    },
+    "3": {
+      lat: "-4.500",
+      lng: "-60.000",
+      name: "Norte",
+      color: "#000000",
+      type: "circle",
+      size: "30",
+      description: "Desigualdade no acesso a serviços básicos, como saneamento e saúde, devido às grandes distâncias e falta de infraestrutura."
+    },
+    "4": {
+      lat: "-20.000",
+      lng: "-45.000",
+      name: "Centro Oeste",
+      color: "#000000",
+      size: "30",
+      type: "circle",
+      description: "Desigualdade ligada à concentração de terras (latifúndios) e ao modelo do agronegócio."
+    }
+  },
+  labels: {
+    BRAC: {
+      name: "Acre",
+      parent_id: "BRAC"
+    },
+    BRAL: {
+      name: "Alagoas",
+      parent_id: "BRAL"
+    },
+    BRAM: {
+      name: "Amazonas",
+      parent_id: "BRAM"
+    },
+    BRAP: {
+      name: "Amapá",
+      parent_id: "BRAP"
+    },
+    BRBA: {
+      name: "Bahia",
+      parent_id: "BRBA"
+    },
+    BRCE: {
+      name: "Ceará",
+      parent_id: "BRCE"
+    },
+    BRDF: {
+      name: "Distrito Federal",
+      parent_id: "BRDF"
+    },
+    BRES: {
+      name: "Espírito Santo",
+      parent_id: "BRES"
+    },
+    BRGO: {
+      name: "Goiás",
+      parent_id: "BRGO"
+    },
+    BRMA: {
+      name: "Maranhão",
+      parent_id: "BRMA"
+    },
+    BRMG: {
+      name: "Minas Gerais",
+      parent_id: "BRMG"
+    },
+    BRMS: {
+      name: "Mato Grosso do Sul",
+      parent_id: "BRMS"
+    },
+    BRMT: {
+      name: "Mato Grosso",
+      parent_id: "BRMT"
+    },
+    BRPA: {
+      name: "Pará",
+      parent_id: "BRPA"
+    },
+    BRPB: {
+      name: "Paraíba",
+      parent_id: "BRPB"
+    },
+    BRPE: {
+      name: "Pernambuco",
+      parent_id: "BRPE"
+    },
+    BRPI: {
+      name: "Piauí",
+      parent_id: "BRPI"
+    },
+    BRPR: {
+      name: "Paraná",
+      parent_id: "BRPR"
+    },
+    BRRJ: {
+      name: "Rio de Janeiro",
+      parent_id: "BRRJ"
+    },
+    BRRN: {
+      name: "Rio Grande do Norte",
+      parent_id: "BRRN"
+    },
+    BRRO: {
+      name: "Rondônia",
+      parent_id: "BRRO"
+    },
+    BRRR: {
+      name: "Roraima",
+      parent_id: "BRRR"
+    },
+    BRRS: {
+      name: "Rio Grande do Sul",
+      parent_id: "BRRS"
+    },
+    BRSC: {
+      name: "Santa Catarina",
+      parent_id: "BRSC"
+    },
+    BRSE: {
+      name: "Sergipe",
+      parent_id: "BRSE"
+    },
+    BRSP: {
+      name: "São Paulo",
+      parent_id: "BRSP"
+    },
+    BRTO: {
+      name: "Tocantins",
+      parent_id: "BRTO"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
